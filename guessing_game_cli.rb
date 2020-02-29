84391b02c5d3e4ccb1af6336cb4caaa288ceb3da
Donny L <donny.landis@gmail.com>
2020-02-29 22:38:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,23 @@
-def run_guessing_game
-  secret = rand(5) + 1
+
+def generate_num
+  rand(5) + 1
+end
+
+def prompt_user
   puts "Guess a number between 1 and 6"
-  un = gets.chomp.strip
+end
+
+def get_guess
+  gets.chomp.strip
+end
+
+def run_guessing_game
+  sn = generate_num
+  prompt_user
+  g = get_guess
   if un == "exit"
     puts "Goodbye!"
-  elsif un == secret
-    puts "You guess the correct number!"
   else
-    puts "Sorry! the computer guessed #{secret}."
+    g == sn ? puts "You guessed the correct number!" : puts "Sorry! the computer guessed #{sn}."
   end
 end
